a4572bc4068094c19cd13ae6116b6cc3c53442f0
Tyler Ashcroft <t.c.ashcroft@gmail.com>
2020-12-03 20:53:09 -0700

    Resolved issue retrieving upgrades

--- t65-cli/src/main/java/com/tcashcroft/t65/cli/client/UpgradeClient.java
+++ t65-cli/src/main/java/com/tcashcroft/t65/cli/client/UpgradeClient.java
@@ -23,7 +23,7 @@ public class UpgradeClient extends T65Client {
     public List<Upgrade> getUpgrades() {
         URI uri = UriComponentsBuilder.fromUri(squadBuilderUrl).pathSegment(CONTEXT_ROOT, "upgrade", "all").build().toUri();
         Upgrade[] upgradeArray = restTemplate.getForObject(uri.toString(), Upgrade[].class);
-        return Stream.of(upgradeArray).sorted().collect(Collectors.toList());
+        return Stream.of(upgradeArray).sorted((u1, u2) -> u1.getNameId().compareTo(u2.getNameId())).collect(Collectors.toList());
     }
 
     public List<Upgrade> getUpgrades(String filter, String id) {
